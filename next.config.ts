ee263713520c4cb3cbe90040800feb3e9a52639e
ChellappanRajan <vrchellappan@gmail.com>
2025-08-21 16:53:04 +0530

    fix: enable SSR support by wrapping nextConfig with withLitSSR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+//ts-ignore
+const withLitSSR = require("@lit-labs/nextjs")();
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withLitSSR(nextConfig);
